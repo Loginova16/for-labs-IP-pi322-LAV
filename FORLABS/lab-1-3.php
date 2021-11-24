81e61c764593e899e8bb71ca585b2fbc3f4257d2
Loginova16 <foolthinks@gmail.com>
2021-11-25 02:37:08 +0500

    Создала php-скрипт lab-1-3.php, в котором создала константу NUM_E

---
+++ FORLABS/lab-1-3.php
@@ -0,0 +1,27 @@
+<div id="bubble">
+  <p><a href="http://f0597837.xsph.ru">Click for OVN Homepage!</a></p>
+</div>
+
+<p> Мат. операция:
+<p>
+<?php
+$NUM_E=(M_E);
+
+echo ('Namber e' . ' = ' . $NUM_E . '<br>');
+
+$num_e1=($NUM_E);
+echo ('$num_e1' . ' = ' . $num_e1 . ' - ');
+echo gettype($num_e1) . '<br>';
+$num_e1 = (string) $num_e1;
+echo ('$num_e1' . ' = ' . $num_e1 . ' - ');
+echo gettype($num_e1) . '<br>';
+$num_e1 = (int) $num_e1;
+echo ('$num_e1' . ' = ' . $num_e1 . ' - ');
+echo gettype($num_e1) . '<br>';
+$num_e1 = (float) $num_e1;
+echo ('$num_e1' . ' = ' . $num_e1 . ' - ');
+echo gettype($num_e1) . '<br>';
+$num_e1 = (string) $num_e1;
+echo ('$num_e1' . ' = ' . $num_e1 . ' - ');
+echo gettype($num_e1) . '<br>';
+?>
