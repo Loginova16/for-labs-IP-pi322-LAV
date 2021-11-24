a77723443215f7442b43973f12b095e8fb8906b0
Loginova16 <foolthinks@gmail.com>
2021-11-25 02:32:46 +0500

    Создала файл lab-1-1.php со скриптом

---
+++ FORLABS/lab-1-1.php
@@ -0,0 +1,15 @@
+<ul>
+Вставь ссылку в поисковик для перехода на главную страиницу сайта
+<li>http://f0597837.xsph.ru</li>
+<?php
+
+ $var1= '3';
+ $var2 = 5;
+ echo $var1 . ' - ' . gettype($var1) . '<br>';
+ print ($var2 . ' - ' . gettype($var2) . '<br>');
+ $var3= 'abc';
+ var_dump ($var3);
+ echo '<br>';
+ $var4=123;
+ var_dump ($var4);
+?>
