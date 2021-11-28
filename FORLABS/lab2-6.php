931dacdb5665f221f6bd78975bad78e61dcb1b1e
Loginova16 <foolthinks@gmail.com>
2021-11-28 17:44:34 +0500

    Написала скрипт для самостоятельной работы ТРИИИ!!!!!!!!

---
+++ FORLABS/lab2-6.php
@@ -0,0 +1,54 @@
+<div id="bubble">
+  <p><a href="http://f0597837.xsph.ru">Click for OVN Homepage!</a></p>
+</div>
+
+
+<?php
+
+$x=10;
+$y=1;
+$v=1;
+$k='levo';
+do {
+    $mass[$x][$y]=$v;
+    $fin=0;
+    if (!$fin AND $k=='levo') {
+    $x--;
+    if ($x<1 OR isset($mass[$x][$y])) { $x++; $k='niz'; }
+    else $fin=1;
+    }
+    if (!$fin AND $k=='niz') {
+    $y++;
+    if ($y>10 OR isset($mass[$x][$y])) { $y--; $k='pravo'; }
+    else $fin=1;
+    }
+    if (!$fin AND $k=='pravo') {
+    $x++;
+    if ($x>10 OR isset($mass[$x][$y])) { $x--; $k='verh'; }
+    else $fin=1;
+    }
+    if (!$fin AND $k=='verh') {
+    $y--;
+    if ($y<1 OR isset($mass[$x][$y])) { $y++; $k='levo'; }
+    else $fin=1;
+    }
+    if (!$fin AND $k=='levo') {
+    $x--;
+    if ($x<1 OR isset($mass[$x][$y])) { $x++; $k='niz'; }
+    else $fin=1;
+    }
+    $v++;
+} while ($v<=100);
+
+$tbl = '';
+for ($y=1;$y<=10;$y++) {
+    $tr = '';
+    for ($x=1;$x<=10;$x++) {
+        $tr .= '<td>'.$mass[$x][$y].'</td>';
+    }
+    $tbl .= '<tr>'.$tr.'</tr>';
+}
+echo '<table>'.$tbl.'</table>';
+
+?>
+  
