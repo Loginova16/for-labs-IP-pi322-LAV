0db84a83fed5322ccd855eec5da112fe02db2284
Loginiva16 <foolthinks@gmail.com>
2021-12-14 13:07:06 +0500

    Документы которые содержат форму с двумя полями для ввода чисел и переключатель, который будет определять, какое действие надо выполнить с числами

---
+++ FORLABS/f3_p.php
@@ -0,0 +1,11 @@
+<body bgcolor="#F08080">
+<?
+if ($_POST["d"]=="plus") {
+ $c=$_POST["a"]+$_POST["b"];
+ echo ("сумма чисел = $c");
+} else {
+ $c=$_POST["a"]*$_POST["b"];
+ echo ("произведение чисел = $c");
+ }
+echo ("<BR> <A href='f3.html'> Вернуться назад </A>");
+?>
