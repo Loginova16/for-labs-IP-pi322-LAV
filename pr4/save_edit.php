2ecca9606825134d13a20445ab509c2436905e91
Loginiva16 <foolthinks@gmail.com>
2021-12-23 19:23:51 +0500

    скрипт для сохранения измегнений

---
+++ pr4/save_edit.php
@@ -0,0 +1,34 @@
+<html> <body>
+  <p><a href="http://f0603071.xsph.ru">Click for OVN Homepage!</a></p>
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'users');
+
+$mysql = @new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if ($mysql ->connect_errno) exit ('Ошибка');
+$mysql->set_charset('utf8');
+
+$mysql->close();
+
+$induction = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+$result = mysqli_query($induction, "SELECT id_user, user_name, user_e_mail
+FROM users");
+
+$users = mysqli_fetch_assoc($result);
+//print_r($users);
+ $zapros="UPDATE users SET user_name='".$_GET['name'].
+"', user_login='".$_GET['login']."', user_password='"
+.$_GET['password']."', user_e_mail='".$_GET['e_mail'].
+"', user_info='".$_GET['info']."' WHERE id_user="
+.$_GET['id'];
+ mysqli_query($induction, $zapros);
+ if (mysqli_affected_rows($induction)>0) {
+ echo 'Все сохранено. <a href="index.php"> Вернуться к списку
+пользователей </a>'; }
+ else { echo 'Ошибка сохранения. <a href="index.php">
+Вернуться к списку пользователей</a> '; }
+?>
+</body> </html>
